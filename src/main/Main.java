32f9cdc0c674d28ee86115e42a1509d3ce49ca0f
U-ADMINIB-66HUGAF\gpclark <gpclark@ADMINIB-66HUGAF.(none)>
2014-03-08 19:01:45 -0500

    grumble

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,81 +3,41 @@ package main;
 
 import java.awt.EventQueue;
 
-
-public class Main {
-	public static void main(String[] args){
-	final ScriptWindowView myview;
-	ScriptWindowViewObserver observer;
-	observer = new ScriptWindowViewObserver();
-	myview = new ScriptWindowView(observer);
+	public class Main {
+		public static void main(String[] args){
+		final ScriptWindowView myview;
+		ScriptWindowViewObserver observer;
+		observer = new ScriptWindowViewObserver();
+		myview = new ScriptWindowView(observer);
+				
+			final Controller mycontroller;
+			ScriptGeneratorModel mymodel;
+	
+			mymodel = new ScriptGeneratorModel();
+			mycontroller = new Controller(mymodel,myview);
+			observer.setController(mycontroller);
+			mymodel.setController(mycontroller);
+			myview.setController(mycontroller);
 			
-		final Controller mycontroller;
-		ScriptGeneratorModel mymodel;
-
-		mymodel = new ScriptGeneratorModel();
-		mycontroller = new Controller(mymodel,myview);
-		observer.setController(mycontroller);
-		mymodel.setController(mycontroller);
-		myview.setController(mycontroller);
-		
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ScriptGeneratorGUI frame = new ScriptGeneratorGUI();
-					frame.setVisible(true);
-					frame.setView(myview);
-					myview.setGui(frame);
-					mycontroller.setGui(frame);
-				} catch (Exception e) {
-					e.printStackTrace();
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						ScriptGeneratorGUI frame = new ScriptGeneratorGUI();
+						frame.setVisible(true);
+						frame.setView(myview);
+						myview.setGui(frame);
+						mycontroller.setGui(frame);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 				}
-			}
-		});
-		
-		
-		
-		
-	}
-}
-=======
-package main;
-
-import java.awt.EventQueue;
-
-
-public class Main {
-	public static void main(String[] args){
-	final ScriptWindowView myview;
-	ScriptWindowViewObserver observer;
-	observer = new ScriptWindowViewObserver();
-	myview = new ScriptWindowView(observer);
+			});
 			
-		final Controller mycontroller;
-		ScriptGeneratorModel mymodel;
-
-		mymodel = new ScriptGeneratorModel();
-		mycontroller = new Controller(mymodel,myview);
-		observer.setController(mycontroller);
-		mymodel.setController(mycontroller);
-		myview.setController(mycontroller);
-		
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ScriptGeneratorGUI frame = new ScriptGeneratorGUI();
-					frame.setVisible(true);
-					frame.setView(myview);
-					myview.setGui(frame);
-					mycontroller.setGui(frame);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		
-		
-		
-		
+			
+			
+			
+		}
 	}
-}
+
+
 >>>>>>> 50449ff1e48a7f941f5ef1bc54501ac8cdae3266
